659cf069cc41c75cb3957f81141021ed4e5d090c
JangHyukJin <wkdgurwls00@naver.com>
2018-10-07 17:23:53 +0900

    Initial Commit

---
+++ Bingo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bingo
+//
+//  Created by JangHyukJin on 07/10/2018.
+//  Copyright © 2018 JangHyukJin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
